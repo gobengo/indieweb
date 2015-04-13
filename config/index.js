6c152ca9322947a020f575f5bbb2c25b759bde95
Benjamin Goering <bengoering@gmail.com>
2015-04-12 21:49:49 -0700

    Add support for requiring HTTPS

--- config/index.js
+++ config/index.js
@@ -5,17 +5,14 @@ var config = module.exports = require('nconf')
     config: {
       describe: "Path to config file to use for config values"
     },
-    port: {
+    PORT: {
       describe: "Port to listen for HTTP traffic on",
-    },
-    'https_only': {
-      describe: "Whether the web server should redirect all http requests to https"
-    },
-    'https_only_trust_x_forwarded_proto': {
-      describe: "Whether the web server should trust request.headers['x-forwarded-proto'] when determining whether a request was sent over HTTPS"
     }
+    // TODO: https.*
   })
-  .env()
+  .env({
+    separator: '_'
+  })
 
 var configFile = config.get('config');
 if (configFile) {
